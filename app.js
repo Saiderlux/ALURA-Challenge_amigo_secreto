ece2074fc5ead4a69a35a7180184f0a893456325
Saiderlux <ferreirasaid55@gmail.com>
2025-08-19 17:02:16 -0600

    Bug fixed in agregarAmigo function and mostrarListaHTML function completed

--- app.js
+++ app.js
@@ -12,15 +12,27 @@ function agregarAmigo(){
     if(input.value==='')
         alert('El cuadro de texto está vacío');
     else{
-        nombresDeAmigos.push(input.value.trim);//Usé trim para eliminar espacios en blanco, al inicio y final de la cadena, que el usuario pueda ingresar
+        nombresDeAmigos.push(input.value.trim());//Usé trim para eliminar espacios en blanco, al inicio y final de la cadena, que el usuario pueda ingresar
     }
 
     input.focus(); // Para que el cursor ya esté en el cuadro de texto, creo que lo  hará más cómodo
     limpiarCuadro();
-    console.log(nombresDeAmigos);
+    console.log(nombresDeAmigos); // ando viendo si sirve
+    mostrarListaHTML();
 }
 
 function limpiarCuadro() {
     document.querySelector('#amigo').value = '';
 }
 
+function mostrarListaHTML(){
+    let lista = document.querySelector('#listaAmigos');
+
+    lista.innerHTML = ""; //limpia la lista, esto evita que se repitan nombres
+
+    for (let i = 0; i < nombresDeAmigos.length; i++) {
+        const li = document.createElement('li'); // crea <li> en el html
+        li.textContent = nombresDeAmigos[i];     // mete el texto al <li>
+        lista.appendChild(li);                   // mete el li<> a la lista
+    }
+}
